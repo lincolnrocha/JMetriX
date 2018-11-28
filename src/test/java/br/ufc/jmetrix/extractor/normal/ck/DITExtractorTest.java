5ffcee560970e78d269ce0fd7b89368e48124436
lucasdourado <lucasdourado@great.ufc.br>
2018-11-27 22:30:10 -0300

    Melhorias no processo de teste.

--- src/test/java/br/ufc/jmetrix/extractor/normal/ck/DITExtractorTest.java
+++ src/test/java/br/ufc/jmetrix/extractor/normal/ck/DITExtractorTest.java
@@ -3,6 +3,7 @@ package br.ufc.jmetrix.extractor.normal.ck;
 import static org.junit.Assert.*;
 
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import br.ufc.jmetrix.model.Dataset;
@@ -26,264 +27,126 @@ public class DITExtractorTest {
 		
 		spoonAPI.process();
 	}
-
-	@Test
-	public void testAbstractAccount() {
+	
+	@Ignore
+	public void test(String classQualifiedName, Metric metric, double expected, double delta) {
+		boolean classFound = false, metricFound = false;
+		
 		for(Suite suite : Dataset.list()) {
-			if(suite.getClassQualifiedName().equals("banksys.account.AbstractAccount")) {
+			if(suite.getClassQualifiedName().equals(classQualifiedName)) {
+				classFound = true;
+				
 				for(Measure measure : suite.getMeasures()) {
-					if(measure.getMetric() == Metric.DIT) {
-						assertEquals(1.0, measure.getValue(), 0.001);
+					if(measure.getMetric() == metric) {
+						metricFound = true;
+						
+						assertEquals(expected, measure.getValue(), delta);
 					}
+				}
+				
+				if(!metricFound) {
+					fail("A classe '" + classQualifiedName + "' não pôde ser avaliada na métrica '" + metric + "'.");
 				}
 			}
 		}
+		
+		if(!classFound) {
+			fail("Não foi possível encontrar a classe '" + classQualifiedName + "'.");
+		}
+	}
+
+	@Test
+	public void testAbstractAccount() {
+		test("banksys.account.AbstractAccount", Metric.DIT, 1.0, 0.001);
 	}
 	
 	@Test
 	public void testOrdinaryAccount() {
-		for(Suite suite : Dataset.list()) {
-			if(suite.getClassQualifiedName().equals("banksys.account.OrdinaryAccount")) {
-				for(Measure measure : suite.getMeasures()) {
-					if(measure.getMetric() == Metric.DIT) {
-						assertEquals(2.0, measure.getValue(), 0.001);
-					}
-				}
-			}
-		}
+		test("banksys.account.OrdinaryAccount", Metric.DIT, 2.0, 0.001);
 	}
 	
 	@Test
 	public void testSavingsAccount() {
-		for(Suite suite : Dataset.list()) {
-			if(suite.getClassQualifiedName().equals("banksys.account.SavingsAccount")) {
-				for(Measure measure : suite.getMeasures()) {
-					if(measure.getMetric() == Metric.DIT) {
-						assertEquals(3.0, measure.getValue(), 0.001);
-					}
-				}
-			}
-		}
+		test("banksys.account.SavingsAccount", Metric.DIT, 3.0, 0.001);
 	}
 	
 	@Test
 	public void testSpecialAccount() {
-		for(Suite suite : Dataset.list()) {
-			if(suite.getClassQualifiedName().equals("banksys.account.SpecialAccount")) {
-				for(Measure measure : suite.getMeasures()) {
-					if(measure.getMetric() == Metric.DIT) {
-						assertEquals(3.0, measure.getValue(), 0.001);
-					}
-				}
-			}
-		}
+		test("banksys.account.SpecialAccount", Metric.DIT, 3.0, 0.001);
 	}
 	
 	@Test
 	public void testTaxAccount() {
-		for(Suite suite : Dataset.list()) {
-			if(suite.getClassQualifiedName().equals("banksys.account.TaxAccount")) {
-				for(Measure measure : suite.getMeasures()) {
-					if(measure.getMetric() == Metric.DIT) {
-						assertEquals(2.0, measure.getValue(), 0.001);
-					}
-				}
-			}
-		}
+		test("banksys.account.TaxAccount", Metric.DIT, 2.0, 0.001);
 	}
 	
 	@Test
 	public void testInsufficientFundsException() {
-		for(Suite suite : Dataset.list()) {
-			if(suite.getClassQualifiedName().equals("banksys.account.exception.InsufficientFundsException")) {
-				for(Measure measure : suite.getMeasures()) {
-					if(measure.getMetric() == Metric.DIT) {
-						assertEquals(3.0, measure.getValue(), 0.001);
-					}
-				}
-			}
-		}
+		test("banksys.account.exception.InsufficientFundsException", Metric.DIT, 3.0, 0.001);
 	}
-	
+
 	@Test
 	public void testNegativeAmountException() {
-		for(Suite suite : Dataset.list()) {
-			if(suite.getClassQualifiedName().equals("banksys.account.exception.NegativeAmountException")) {
-				for(Measure measure : suite.getMeasures()) {
-					if(measure.getMetric() == Metric.DIT) {
-						assertEquals(3.0, measure.getValue(), 0.001);
-					}
-				}
-			}
-		}
+		test("banksys.account.exception.NegativeAmountException", Metric.DIT, 3.0, 0.001);
 	}
 	
 	@Test
 	public void testBankController() {
-		for(Suite suite : Dataset.list()) {
-			if(suite.getClassQualifiedName().equals("banksys.control.BankController")) {
-				for(Measure measure : suite.getMeasures()) {
-					if(measure.getMetric() == Metric.DIT) {
-						assertEquals(1.0, measure.getValue(), 0.001);
-					}
-				}
-			}
-		}
+		test("banksys.control.BankController", Metric.DIT, 1.0, 0.001);
 	}
 	
 	@Test
 	public void testBankTransactionException() {
-		for(Suite suite : Dataset.list()) {
-			if(suite.getClassQualifiedName().equals("banksys.control.exception.BankTransactionException")) {
-				for(Measure measure : suite.getMeasures()) {
-					if(measure.getMetric() == Metric.DIT) {
-						assertEquals(3.0, measure.getValue(), 0.001);
-					}
-				}
-			}
-		}
+		test("banksys.control.exception.BankTransactionException", Metric.DIT, 3.0, 0.001);
 	}
 	
 	@Test
 	public void testIncompatibleAccountException() {
-		for(Suite suite : Dataset.list()) {
-			if(suite.getClassQualifiedName().equals("banksys.control.exception.IncompatibleAccountException")) {
-				for(Measure measure : suite.getMeasures()) {
-					if(measure.getMetric() == Metric.DIT) {
-						assertEquals(4.0, measure.getValue(), 0.001);
-					}
-				}
-			}
-		}
+		test("banksys.control.exception.IncompatibleAccountException", Metric.DIT, 4.0, 0.001);
 	}
 	
 	@Test
 	public void testAccountVector() {
-		for(Suite suite : Dataset.list()) {
-			if(suite.getClassQualifiedName().equals("banksys.persistence.AccountVector")) {
-				for(Measure measure : suite.getMeasures()) {
-					if(measure.getMetric() == Metric.DIT) {
-						assertEquals(1.0, measure.getValue(), 0.001);
-					}
-				}
-			}
-		}
+		test("banksys.persistence.AccountVector", Metric.DIT, 1.0, 0.001);
 	}
 	
 	@Test
 	public void testAccountXStream() {
-		for(Suite suite : Dataset.list()) {
-			if(suite.getClassQualifiedName().equals("banksys.persistence.AccountXStream")) {
-				for(Measure measure : suite.getMeasures()) {
-					if(measure.getMetric() == Metric.DIT) {
-						assertEquals(1.0, measure.getValue(), 0.001);
-					}
-				}
-			}
-		}
-	}
-	
-	@Test
-	public void testIAccountRepository() {
-		for(Suite suite : Dataset.list()) {
-			if(suite.getClassQualifiedName().equals("banksys.persistence.IAccountRepository")) {
-				for(Measure measure : suite.getMeasures()) {
-					if(measure.getMetric() == Metric.DIT) {
-						assertEquals(0.0, measure.getValue(), 0.001);
-					}
-				}
-			}
-		}
+		test("banksys.persistence.AccountXStream", Metric.DIT, 1.0, 0.001);
 	}
 	
 	@Test
 	public void testAccountCreationException() {
-		for(Suite suite : Dataset.list()) {
-			if(suite.getClassQualifiedName().equals("banksys.persistence.exception.AccountCreationException")) {
-				for(Measure measure : suite.getMeasures()) {
-					if(measure.getMetric() == Metric.DIT) {
-						assertEquals(4.0, measure.getValue(), 0.001);
-					}
-				}
-			}
-		}
+		test("banksys.persistence.exception.AccountCreationException", Metric.DIT, 4.0, 0.001);
 	}
 	
 	@Test
 	public void testAccountDeletionException() {
-		for(Suite suite : Dataset.list()) {
-			if(suite.getClassQualifiedName().equals("banksys.persistence.exception.AccountDeletionException")) {
-				for(Measure measure : suite.getMeasures()) {
-					if(measure.getMetric() == Metric.DIT) {
-						assertEquals(4.0, measure.getValue(), 0.001);
-					}
-				}
-			}
-		}
+		test("banksys.persistence.exception.AccountDeletionException", Metric.DIT, 4.0, 0.001);
 	}
 	
 	@Test
 	public void testAccountNotFoundException() {
-		for(Suite suite : Dataset.list()) {
-			if(suite.getClassQualifiedName().equals("banksys.persistence.exception.AccountNotFoundException")) {
-				for(Measure measure : suite.getMeasures()) {
-					if(measure.getMetric() == Metric.DIT) {
-						assertEquals(4.0, measure.getValue(), 0.001);
-					}
-				}
-			}
-		}
+		test("banksys.persistence.exception.AccountNotFoundException", Metric.DIT, 4.0, 0.001);
 	}
 	
 	@Test
 	public void testExistingAccountException() {
-		for(Suite suite : Dataset.list()) {
-			if(suite.getClassQualifiedName().equals("banksys.persistence.exception.ExistingAccountException")) {
-				for(Measure measure : suite.getMeasures()) {
-					if(measure.getMetric() == Metric.DIT) {
-						assertEquals(3.0, measure.getValue(), 0.001);
-					}
-				}
-			}
-		}
+		test("banksys.persistence.exception.ExistingAccountException", Metric.DIT, 3.0, 0.001);
 	}
-	
+
 	@Test
 	public void testFlushException() {
-		for(Suite suite : Dataset.list()) {
-			if(suite.getClassQualifiedName().equals("banksys.persistence.exception.FlushException")) {
-				for(Measure measure : suite.getMeasures()) {
-					if(measure.getMetric() == Metric.DIT) {
-						assertEquals(4.0, measure.getValue(), 0.001);
-					}
-				}
-			}
-		}
+		test("banksys.persistence.exception.FlushException", Metric.DIT, 4.0, 0.001);
 	}
 	
 	@Test
 	public void testPersistenceException() {
-		for(Suite suite : Dataset.list()) {
-			if(suite.getClassQualifiedName().equals("banksys.persistence.exception.PersistenceException")) {
-				for(Measure measure : suite.getMeasures()) {
-					if(measure.getMetric() == Metric.DIT) {
-						assertEquals(3.0, measure.getValue(), 0.001);
-					}
-				}
-			}
-		}
+		test("banksys.persistence.exception.PersistenceException", Metric.DIT, 3.0, 0.001);
 	}
 	
 	@Test
 	public void testATM24H() {
-		for(Suite suite : Dataset.list()) {
-			if(suite.getClassQualifiedName().equals("banksys.view.ATM24H")) {
-				for(Measure measure : suite.getMeasures()) {
-					if(measure.getMetric() == Metric.DIT) {
-						assertEquals(1.0, measure.getValue(), 0.001);
-					}
-				}
-			}
-		}
+		test("banksys.view.ATM24H", Metric.DIT, 1.0, 0.001);
 	}
 }
